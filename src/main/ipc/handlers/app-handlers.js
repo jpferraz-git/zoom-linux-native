627aa27d69753401a667e35951504a929c15e406
jpferraz-git <jpferraz554@gmail.com>
2026-08-25 19:27:11 -0300

    refactor: add JSDoc documentation, refine CSP filters, and improve error logging in security and IPC modules

--- src/main/ipc/handlers/app-handlers.js
+++ src/main/ipc/handlers/app-handlers.js
@@ -4,6 +4,14 @@ const { ipcMain } = require('electron');
 const { CHANNELS } = require('../channels');
 const { ZOOM_WEB_CLIENT_URL } = require('../../config/constants');
 
+/**
+ * Registra handlers IPC do domínio "app" no main process.
+ *
+ * Atualmente expõe apenas o canal APP_RELOAD, que recarrega o Zoom Web Client
+ * na janela principal. Chamado uma vez em index.js durante a inicialização.
+ *
+ * @param {() => Electron.BrowserWindow | null} getMainWindow
+ */
 function registerAppHandlers(getMainWindow) {
   ipcMain.handle(CHANNELS.APP_RELOAD, () => {
     const win = getMainWindow();
